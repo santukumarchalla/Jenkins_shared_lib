7fba2d2c94a1a3ee92d9de76f799fb57dba065e2
santukumarchalla <109782213+santukumarchalla@users.noreply.github.com>
2023-12-02 14:20:39 +0530

    Update jfrogUpload.groovy

--- vars/jfrogUpload.groovy
+++ vars/jfrogUpload.groovy
@@ -1,4 +1,4 @@
 def call() {
   echo '------executing jfrog'
-  sh """ curl -X PUT -u admin:Admin123-T /var/lib/jenkins/workspace/JfrogArtf/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://3.142.47.84:8082
+  sh """ curl -X PUT -u admin:Admin@123-T /var/lib/jenkins/workspace/JfrogArtf/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://18.212.17.198:8081
   }
